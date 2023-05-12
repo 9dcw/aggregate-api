7b865f82311c221853737870fa0159b84d366544
9dcw <davecwright@gmail.com>
2023-05-12 16:53:07 -0400

    test

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 def test():
 
-  return 'test'
+    return 'test'
 
 
 @app.route('/add', methods=['POST'])
 def add():
-  data = request.get_json()
-  num1 = data['num1']
-  num2 = data['num2']
-  result = num1 + num2
-  return jsonify({'result': result})
+    data = request.get_json()
+    num1 = data['num1']
+    num2 = data['num2']
+    result = num1 + num2
+    return jsonify({'result': result})
 
 
 @app.route('/agg', methods=['POST'])
 def aggregate_start():
-  result = 'test'
-  return jsonify({'result': result})
+    result = 'test'
+    return jsonify({'result': result})
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
