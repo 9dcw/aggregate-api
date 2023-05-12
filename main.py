2e13f4270d592da40ca8b670502fe0d49d916adc
9dcw <davecwright@gmail.com>
2023-05-12 13:48:10 -0400

    test

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI
+from flask import Flask, request, jsonify
+from aggregate import build, qd
 
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app = Flask(__name__)
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.route('/add', methods=['POST'])
+def add():
+  data = request.get_json()
+  num1 = data['num1']
+  num2 = data['num2']
+  result = num1 + num2
+  return jsonify({'result': result})
+
+
+@app.route('/agg', methods=['POST'])
+def aggregate_start():
+  result = 'test'
+  return jsonify({'result': result})
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=5000)
